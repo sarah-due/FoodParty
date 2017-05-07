aff6584052be399e5cc456b5fcd52b9667a275f2
Sarah Dueweke <sdueweke@gmail.com>
2017-05-07 18:37:39 +1200

    Add styling and more snacks to json file

--- routes.js
+++ routes.js
@@ -2,10 +2,12 @@
 var express = require('express')
 var router = express.Router()
 var food = require('./food.json')
+var bodyParser = require('body-parser')
+var fs = require('fs')
 
 var app = express()
 
-var viewFood =
+var foodList =
 { title: "Food and Stuff!",
   food: food
 }
@@ -15,17 +17,22 @@ router.get('/', function (req, res) {
 })
 
 router.get('/food',(req,res)=>{
-  res.render('foods/index', viewFood)
+  res.render('foods/index', foodList)
 })
 
-//
-// router.get('/:id', function (req, res) {
-//   // ...
-// })
+router.get('/food/:id', (req, res)=> {
+  var id = req.params.id
 
-//
-// router.post('/', function (req, res) {
-//   // ...
-// })
+  food.find(function(item){
+    if (item.id == id) {
+      res.render('foods/viewfood', item)
+    }
+  })
+})
+
+router.get('/addrecipe', (req, res) => {
+  res.render('foods/newrecipe')
+})
+
 
 module.exports = router
